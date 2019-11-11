1bb39d653aea94a09c51e8dd176a5fe93858cd08
Pavel Suchman <pavels@datascience.co.il>
2019-11-11 11:56:59 +0200

    Make webdriver path configurable; support not-http protocols

--- show_me_that_website.py
+++ show_me_that_website.py
@@ -7,19 +7,21 @@ parser.add_argument('url', type=str, metavar='', help='which page to screenshot'
 parser.add_argument('-b', '--bucket', type=str, metavar='', help='which bucket to upload to')
 args = parser.parse_args()
 s3 = boto3.resource('s3')
+DRIVER_PATH="./geckodriver"
 
 
 def getScreenshot(link):
     options = Options()
     options.headless = True
-    with webdriver.Firefox(options=options, executable_path="geckodriver.exe") as driver:
+    screenshot_fn = f"{link.split('://')[1]}.png"
+    with webdriver.Firefox(options=options, executable_path=DRIVER_PATH) as driver:
         driver.get(link)
         height = driver.execute_script("return document.body.scrollHeight")
         width = driver.execute_script("return document.body.scrollWidth")
         driver.set_window_size(width, height)
         time.sleep(2)
-        driver.save_screenshot(f"{link[7:]}.png")
-    return f"{link[7:]}.png"
+        driver.save_screenshot(screenshot_fn)
+    return screenshot_fn
 
 
 def throwToS3(file, bucket):
